533d5bdd342b1f0c817a174d4de184b8e40a116f
Alex Kalik <alexandre.kalik@gmail.com>
2023-04-11 14:52:29 +0300

    ver 3.0

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,27 @@
 let formElement = document.querySelector('.popup__form');
 let nameInput = document.querySelector('#name');
 let jobInput = document.querySelector('#description');
-
+let profilename = document.querySelector('.profile__name');
+let profileabout = document.querySelector('.profile__about');
+let nameFilled;
+let aboutFilled;
+const editButtion = document.querySelector('.profile__btn-edit');
+const popup = document.querySelector('.popup');
+const closeButtion = document.querySelector('.popup__close');
+const submitButtion = document.querySelector('.popup__btn-save');
 
 
 function handleFormSubmit (evt) {
     evt.preventDefault(); 
 
     // Получите значение полей jobInput и nameInput из свойства value
-    a=nameInput.value;
-    b=jobInput.value;
+    nameFilled=nameInput.value;
+    aboutFilled=jobInput.value;
 
-    console.log (a, b);
+    
     // Выберите элементы, куда должны быть вставлены значения полей
-
-    let profilename = document.querySelector('.profile__name');
-    let profileabout = document.querySelector('.profile__about');
-    profilename.textContent = a;
-    profileabout.textContent = b;
+    profilename.textContent = nameFilled;
+    profileabout.textContent = aboutFilled;
     
 
     // Вставьте новые значения с помощью textContent
@@ -26,15 +30,14 @@ function handleFormSubmit (evt) {
 formElement.addEventListener('submit', handleFormSubmit);
 
 
-
-
-const editButtion = document.querySelector('.profile__btn-edit');
-const popup = document.querySelector('.popup');
-const closeButtion = document.querySelector('.popup__close');
  
  function open() {
   popup.classList.add('popup_opened');
+
+  nameInput.value = profilename.textContent;
+  jobInput.value = profileabout.textContent;
   
+
 }
 
 function close() {
@@ -44,7 +47,7 @@ function close() {
  
  editButtion.addEventListener('click', open);
  closeButtion.addEventListener('click', close);
- 
+ submitButtion.addEventListener('click', close);
 
  
 
